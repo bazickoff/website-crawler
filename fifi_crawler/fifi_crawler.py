543df7f331d2847315f10715c1cbfa5aeced4092
Vandana Iyer <vandy.iyer@gmail.com>
2020-02-02 23:40:10 -0800

    resolving conflicts

--- fifi_crawler/fifi_crawler.py
+++ fifi_crawler/fifi_crawler.py
@@ -5,23 +5,13 @@ import logging
 import pandas as pd
 from collections import defaultdict
 
-<<<<<<< HEAD
-from fifi_log import start_logging
-
-=======
->>>>>>> 74b5f874cafe3fee806cc964559997a60cae2ada
-
 class FiFiCrawler(object):
   """
    A crawler to download Find it Fix it (FiFi) images using URLs from the dataset
    """
   # Constants
-<<<<<<< HEAD
-  UNDERSCORE = '_'
-  logger = logging.getLogger(__name__)
-=======
+
   NAME_SEPARATOR = '_'
->>>>>>> 74b5f874cafe3fee806cc964559997a60cae2ada
 
   def __init__(self, filename, image_column_name, service_request_column_name, parent_dir):
     """
@@ -30,16 +20,6 @@ class FiFiCrawler(object):
     :param filename: Name of the file that will be used for crawling.
     :param image_column_name: Column name of the field that has URLs of images.
     :param service_request_column_name: Column name of the Service Request Number.
-<<<<<<< HEAD
-    :param parent_dir: Parent directory where images for each category will be downloaded.
-    """
-
-    self.parent_dir = parent_dir  # To DO: If ends with /, remove /
-    self.image_column_name = image_column_name
-    self.service_request_column_name = service_request_column_name
-    self.xls = pd.ExcelFile(filename)
-    self.fifi_dict = defaultdict.fromkeys(xls.sheet_names) # Excel file sheet names map to categories
-=======
     :param parent_dir: Name of the parent directory where images for each category will be downloaded.
     """
     self.logger = logging.getLogger(__name__)
@@ -49,7 +29,6 @@ class FiFiCrawler(object):
     self.service_request_column_name = service_request_column_name
     self.xls = pd.ExcelFile(filename)
     self.fifi_dict = defaultdict.fromkeys(self.xls.sheet_names) # Excel file sheet names map to categories
->>>>>>> 74b5f874cafe3fee806cc964559997a60cae2ada
 
 
   def crawl(self):
@@ -67,13 +46,8 @@ class FiFiCrawler(object):
     Populates dataframes of each category into the fifi dictionary
     """
 
-<<<<<<< HEAD
-    for name in self.fifi_dict.keys(self):
-      self.fifi_dict[name] = pd.read_excel(xls, name)
-=======
     for name in self.fifi_dict.keys():
       self.fifi_dict[name] = pd.read_excel(self.xls, name)
->>>>>>> 74b5f874cafe3fee806cc964559997a60cae2ada
 
 
   def create_directories(self):
@@ -81,26 +55,15 @@ class FiFiCrawler(object):
     Creates separate folders for each category if it does not exist.
     """
 
-<<<<<<< HEAD
-    for key, df in fifi_dict.items():
-      i = 0
-      directory_path = self.parent_dir + key
-=======
     for key, df in self.fifi_dict.items():
       i = 0
       directory_path = os.path.join(self.parent_dir, key)
->>>>>>> 74b5f874cafe3fee806cc964559997a60cae2ada
 
       try:
         if not os.path.exists(directory_path):
           os.makedirs(directory_path)
-<<<<<<< HEAD
-      except OSERROR:
-        logger.error('Error while trying to create directory', exc_info=True)
-=======
       except os.error:
         self.logger.error('Error while trying to create directory', exc_info=True)
->>>>>>> 74b5f874cafe3fee806cc964559997a60cae2ada
 
 
   def download_images_by_category(self):
@@ -108,18 +71,11 @@ class FiFiCrawler(object):
     Downloads images in every category.
     """
 
-<<<<<<< HEAD
-    for category, df in fifi_dict.items():
-      df = df[df['Photo'].notnull()] # removes rows without URLs
-      for row in df:
-          self.download_image(category, row[self.image_column_name], row[self.service_request_column_name])
-=======
     for category, df in self.fifi_dict.items():
       df = df[df['Photo'].notnull()] # removes rows without URLs
       i = 0
       for (idx, row) in df.iterrows():
           self.download_image(category, row[self.image_column_name], row[self.service_request_column_name], i)
->>>>>>> 74b5f874cafe3fee806cc964559997a60cae2ada
           i = i + 1
 
   def download_image(self, category, image_url, service_request_number, index):
@@ -131,19 +87,6 @@ class FiFiCrawler(object):
       :param index:
       """
 
-<<<<<<< HEAD
-      file_extension = get_file_extension(image_url)
-      if file_extension:
-        local_file_path = FiFiCrawler.build_local_file_path(category, service_request_number, index, file_extension)
-
-        try:
-          urllib.request.urlretrieve(image_url, local_file_path)
-        except (urllib2.URLError, IOError) as e:
-          logger.error("Error downloading image " + image_url +  "with filename: " + file_name, , exc_info=True)
-          # raise e
-        except e:
-          logger.error("Error while downloading image with service_request_number " + service_request_number " and category " + category, exc_info=True)
-=======
       file_extension = self.get_file_extension(image_url)
       if file_extension:
         local_file_path = self.build_local_file_path(category, service_request_number, index, file_extension)
@@ -157,18 +100,13 @@ class FiFiCrawler(object):
         except (Exception) as e:
           self.logger.error("Error downloading image " + image_url +  " from category: "
                             +  category + " with service request number: " + service_request_number + local_file_path, exc_info=True)
->>>>>>> 74b5f874cafe3fee806cc964559997a60cae2ada
 
 
   def get_file_extension(self, url):
     """
     Extracts extension of the image from the URL
-<<<<<<< HEAD
-    :param url:
-=======
 
     :param url: Link to download the image.
->>>>>>> 74b5f874cafe3fee806cc964559997a60cae2ada
     :return file_extension: A string containing the extension of file. Ex" '.jpg'
     """
 
@@ -176,33 +114,7 @@ class FiFiCrawler(object):
     filename, file_extension = os.path.splitext(filename_with_extension)
     return file_extension
 
-<<<<<<< HEAD
-  @staticmethod
-  def build_local_file_path(category, service_request_number, index, file_extension):
-    """
 
-    """
-
-    file_name = category \
-                + self.UNDERSCORE \
-                + service_request_number \
-                + self.UNDERSCORE \
-                + index \
-                + self.UNDERSCORE \
-                + file_extension
-
-    return os.path.join(category, file_name)
-
-if __name__ == '__main__':
-  # TO-DO: Put the config in a json file and read from it. Refer: https://fangpenlin.com/posts/2012/08/26/good-logging-practice-in-python/
-  logging.basicConfig(filename='fifi_crawler.log', 
-                      filemode='w', 
-                      level=logging.DEBUG, 
-                      format='%(levelname)s:%(asctime)s:%(message)s', 
-                      datefmt='%m/%d/%Y %I:%M:%S %p')
-
-  crawler = FiFiCrawler('fifi_data.xlsx', 'Photo', 'Service Request Number', 'data')
-=======
   def build_local_file_path(self, category, service_request_number, index, file_extension):
     """
     Generates a file name and builds a relative path.
@@ -233,6 +145,3 @@ if __name__ == '__main__':
 
   crawler = FiFiCrawler('fifi_data.xlsx', 'Photo', 'Service Request Number', 'data')
   crawler.crawl()
->>>>>>> 74b5f874cafe3fee806cc964559997a60cae2ada
-
-
